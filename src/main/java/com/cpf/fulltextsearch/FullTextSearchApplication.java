5083f6df286987eb6ac08ee8ae0be1b021953d08
ffpffcpf <429650229@qq.com>
2017-11-21 19:10:25 +0800

    init project

---
+++ src/main/java/com/cpf/fulltextsearch/FullTextSearchApplication.java
@@ -0,0 +1,12 @@
+package com.cpf.fulltextsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullTextSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullTextSearchApplication.class, args);
+	}
+}
