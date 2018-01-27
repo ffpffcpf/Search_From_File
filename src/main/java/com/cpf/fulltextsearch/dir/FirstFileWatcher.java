c55435e46d3febee443857caa12e8da468ec1fd9
ffpffcpf <429650229@qq.com>
2018-01-27 13:43:06 +0800

    [PengFei] add simple file watcher

--- src/main/java/com/cpf/fulltextsearch/dir/FirstFileWatcher.java
+++ src/main/java/com/cpf/fulltextsearch/dir/FirstFileWatcher.java
@@ -1,4 +1,45 @@
 package com.cpf.fulltextsearch.dir;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+
 public class FirstFileWatcher {
+
+    private static final Logger log = LoggerFactory.getLogger(FirstFileWatcher.class);
+
+    private WatchService watchService;
+
+    private WatchKey watchKey;
+
+    public FirstFileWatcher() {
+        try {
+            watchService = FileSystems.getDefault().newWatchService();
+            Path des = Paths.get("D:\\file_watcher_test");
+            des.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+
+    public void handleEvent() throws InterruptedException {
+        while (true){
+            WatchKey watchKey = watchService.take();
+
+            for (WatchEvent<?>  event : watchKey.pollEvents()){
+
+                Path filename = ((WatchEvent<Path>)event).context();
+                log.info("=================================="+event.kind().name());
+                log.info("----------------------------------"+filename.toString());
+            }
+
+            watchKey.reset();
+        }
+
+    }
+
 }
--- src/test/java/com/cpf/fulltextsearch/watcher/FirstFileWatcherTest.java
+++ src/test/java/com/cpf/fulltextsearch/watcher/FirstFileWatcherTest.java
@@ -1,4 +1,14 @@
 package com.cpf.fulltextsearch.watcher;
 
+import com.cpf.fulltextsearch.dir.FirstFileWatcher;
+import org.junit.Test;
+
 public class FirstFileWatcherTest {
+
+    @Test
+    public void testHandle() throws InterruptedException {
+        FirstFileWatcher watcher = new FirstFileWatcher();
+        watcher.handleEvent();
+    }
+
 }
